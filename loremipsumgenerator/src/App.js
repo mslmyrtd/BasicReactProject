7e2c749b039dc0e1189b799950e86dbb53194946
mslmyrtd <mslmyrtd@gmail.com>
2022-12-18 23:10:22 +0300

    added form and classes for ui

--- loremipsumgenerator/src/App.js
+++ loremipsumgenerator/src/App.js
@@ -1,10 +1,24 @@
+import React, { useState } from 'react'
+import data from './data'
 function App() {
+  const [text, setText] = useState(data)
+  const handleGenerates = () => {}
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>hello</p>
-      </header>
-    </div>
+    <section>
+      <div className='section-center'>
+        <h3>TIRED OF BORING LOREM IPSUM?</h3>
+        <form className='lorem-form'>
+          <label htmlFor='paragraphs' className='label'>
+            Paragraphs:
+          </label>
+          <input type='number' id='paragraphs' />
+          <button type='submit' className='btn' onClick={handleGenerates}>
+            Generate
+          </button>
+        </form>
+        <p>{text}</p>
+      </div>
+    </section>
   )
 }
 
