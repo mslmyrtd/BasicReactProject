1a50142309c57faf94d8862fa73f597af995d8be
mslmyrtd <mslmyrtd@gmail.com>
2022-12-19 21:41:56 +0300

    end pf project

--- colorgenerator/src/App.js
+++ colorgenerator/src/App.js
@@ -12,7 +12,7 @@ function App() {
     try {
       let colors = new Values(color).all(10)
       setList(colors)
-      console.log(list)
+      setError(false)
     } catch (error) {
       setError(true)
       console.log(error)
@@ -20,20 +20,22 @@ function App() {
     setColor('')
   }
   return (
-    <section className='container'>
-      <h3>Color generator</h3>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='#f15025'
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-          className={`${error ? 'error' : null}`}
-        />
-        <button className='btn' type='submit'>
-          Submit
-        </button>
-      </form>
+    <>
+      <section className='container'>
+        <h3>Color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='#f15025'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            Submit
+          </button>
+        </form>
+      </section>
       <section className='colors'>
         {list.map((color, index) => {
           return (
@@ -46,7 +48,7 @@ function App() {
           )
         })}
       </section>
-    </section>
+    </>
   )
 }
 
