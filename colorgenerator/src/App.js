423d7e89f5ae10f5a58fcfd4fe2266e028611ac4
mslmyrtd <mslmyrtd@gmail.com>
2022-12-19 16:20:30 +0300

    function added and valuesjs used

--- colorgenerator/src/App.js
+++ colorgenerator/src/App.js
@@ -1,15 +1,52 @@
 import React, { useState } from 'react'
 import SingleColor from './SingleColor'
-
 import Values from 'values.js'
 
 function App() {
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#f15025').all(10))
+  console.log(list)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+      console.log(list)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+    setColor('')
+  }
   return (
-    <div className='container'>
+    <section className='container'>
       <h3>Color generator</h3>
-      <input type='text' />
-      <button className='btn'>Submit</button>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='#f15025'
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          className={`${error ? 'error' : null}`}
+        />
+        <button className='btn' type='submit'>
+          Submit
+        </button>
+      </form>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              {...color}
+              key={index}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
+    </section>
   )
 }
 
