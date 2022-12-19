e4458e72a10561f45781c6721a06a2bc467c63f2
mslmyrtd <mslmyrtd@gmail.com>
2022-12-19 11:09:16 +0300

    added function for generate

--- loremipsumgenerator/src/App.js
+++ loremipsumgenerator/src/App.js
@@ -1,8 +1,16 @@
 import React, { useState } from 'react'
 import data from './data'
 function App() {
-  const [text, setText] = useState(data)
-  const handleGenerates = () => {}
+  const [text, setText] = useState([])
+  const [count, setCount] = useState(0)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let amount = parseInt(count)
+    if (amount <= 0) amount = 1
+    if (count > 8) amount = 8
+    setText(data.slice(0, amount))
+  }
+
   return (
     <section>
       <div className='section-center'>
@@ -11,12 +19,23 @@ function App() {
           <label htmlFor='paragraphs' className='label'>
             Paragraphs:
           </label>
-          <input type='number' id='paragraphs' />
-          <button type='submit' className='btn' onClick={handleGenerates}>
+          <input
+            type='number'
+            id='paragraphs'
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <button type='submit' className='btn' onClick={handleSubmit}>
             Generate
           </button>
         </form>
-        <p>{text}</p>
+        {text.map((item, index) => {
+          return (
+            <div key={index}>
+              <p>{item}</p>
+            </div>
+          )
+        })}
       </div>
     </section>
   )
