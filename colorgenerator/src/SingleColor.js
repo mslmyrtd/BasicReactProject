e6f037e33838b985460d6cd18f4ebef06ce3f56c
mslmyrtd <mslmyrtd@gmail.com>
2022-12-19 21:41:39 +0300

    added navgitor.clipboard for copy text

--- colorgenerator/src/SingleColor.js
+++ colorgenerator/src/SingleColor.js
@@ -16,9 +16,14 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
     <article
       className={`color ${index > 10 && 'color-light'}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true)
+        navigator.clipboard.writeText(hexValue)
+      }}
     >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>#{hexValue}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   )
 }
