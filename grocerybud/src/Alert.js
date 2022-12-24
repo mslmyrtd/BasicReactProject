730aa6605787faffd0f31bf8f9e0f91571c6dd57
mslmyrtd <mslmyrtd@gmail.com>
2022-12-24 23:26:13 +0300

    added useeffect for remove alert after show and sended props

--- grocerybud/src/Alert.js
+++ grocerybud/src/Alert.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({ type, msg }) => {
+const Alert = ({ type, msg, removeAlert }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert()
+    }, 1500)
+    return () => clearTimeout(timeout)
+  }, [])
   return <p className={`alert alert-${type}`}>{msg}</p>
 }
 
