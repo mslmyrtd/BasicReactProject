c9692cc3883494e2ed8217989cfea2a8ae21b29a
mslmyrtd <mslmyrtd@gmail.com>
2022-12-24 21:38:02 +0300

    added useState hooks

--- grocerybud/src/App.js
+++ grocerybud/src/App.js
@@ -3,7 +3,18 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+  return (
+    <section className='section-center'>
+      <div className='grocery-container'>
+        <List />
+      </div>
+    </section>
+  )
 }
 
 export default App
