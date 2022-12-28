e21fb6ba3df92835dbcf8471cdffcd7373efb553
mslmyrtd <mslmyrtd@gmail.com>
2022-12-28 16:31:26 +0300

    created structure of navbar and added index.css

---
+++ Navbar/src/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { FaBars, FaTwitter } from 'react-icons/fa'
+import { links, social } from './data'
+import logo from './logo.svg'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <button className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container show-container'>
+          <ul className='links'>
+            {links.map((link) => {
+              return (
+                <li key={link.id}>
+                  <a href={link.url}>{link.text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'></ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
