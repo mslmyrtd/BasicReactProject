1638e36ade078c6af6919c2c380c7db4dc7d1d02
mslmyrtd <mslmyrtd@gmail.com>
2023-01-01 19:51:56 +0300

    sidebar structure created

--- sideBarandModal/src/Sidebar.js
+++ sideBarandModal/src/Sidebar.js
@@ -4,7 +4,38 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return (
+    <aside className={`sidebar`}>
+      <div className='sidebar-header'>
+        <img src={logo} alt='logo' className='logo' />
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((link) => {
+          const { url, text, id, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+      <ul className='social-icons'>
+        {social.map((link) => {
+          const { url, id, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
+  )
 }
 
 export default Sidebar
