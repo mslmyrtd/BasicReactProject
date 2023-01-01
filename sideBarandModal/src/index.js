e54967054b2884dd7b660a7407cd31dd212285af
mslmyrtd <mslmyrtd@gmail.com>
2023-01-01 19:51:42 +0300

    index.js wrapped with provider

--- sideBarandModal/src/index.js
+++ sideBarandModal/src/index.js
@@ -7,8 +7,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <AppProvider>
-    </AppProvider> */}
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 )
